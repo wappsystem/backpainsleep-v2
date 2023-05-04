c61b64628a4aaa5e28b90154ccea14c00e9cbad8
Gunnar Unger <gunnar.unger@gmail.com>
2023-05-04 15:17:15 +1000

    update

--- assets/js/form.01.js
+++ assets/js/form.01.js
@@ -2,15 +2,14 @@
 var m=$vm.module_list['__MODULE__'];
 if(m.prefix==undefined) m.prefix="";
 if(m.change_status==undefined) m.change_status=0;
-m.options={self:m.self};
 //-------------------------------------
 m.load=function(){
     //$('#D__ID').scrollTop(0);
     //$(window).scrollTop(0);
     $('#F__ID')[0].reset();
     $('#submit__ID').show();
-    $('#draft__ID').show();
     $('#delete__ID').hide(); if(m.input!=undefined && m.input.record!=undefined && m.input.record._id!==undefined) $('#delete__ID').show();
+    console.log(JSON.stringify(m.input))
     if(m.input!=undefined) $vm.deserialize(m.input.record,'#F__ID');
 }
 //-------------------------------
@@ -39,97 +38,6 @@ m.set_file_link=function(tag){
         $('#x_'+tag+'__ID').hide();
         record.Data[tag]="";
     })
-}
-//-------------------------------
-m.set_file_link_s0=function(tag){
-    $('#link_'+tag+'__ID').html("");
-    $('#x_'+tag+'__ID').hide();
-    var record=m.input.record;
-    if(record==undefined) return;
-    var filename=record.Data[tag];
-    if(filename==undefined){
-        filename="";
-    }
-    else{
-        $('#x_'+tag+'__ID').show();
-    }
-    $('#link_'+tag+'__ID').html(filename);
-    $('#link_'+tag+'__ID').off().on('click',function(){
-        if(record._id!==undefined && filename!=""){
-            var rid=record.UID;
-            var done=0;
-            var get_file_from_server=function(){
-                if(done==0){
-                    $vm.request({api:m.api,cmd:"file",id:rid,table:m.Table,uid:record.UID, field:tag,filename:filename,options:m.options},function(res, textStatus, xhr){
-                        try{
-                            if(res=="404"){
-                                alert("No such file.");
-                                return;
-                            };
-                        }
-                        catch(e){}
-                        if('caches' in window){
-                            caches.open('VM').then(cache => {
-                                var aHeaders = new Headers();
-                                aHeaders.append('last-modifie',xhr.getResponseHeader('last-modified'));
-                                var rs=new Response(res, { "headers" :aHeaders} );
-                                cache.put(m.Table+"-"+rid+"-"+tag,rs);
-                                return;
-                            })
-                        }
-                        var link=document.createElement('a');
-                        var url = window.URL || window.webkitURL;
-                        link.href=url.createObjectURL(res);
-                        link.download=filename;
-                        link.click();
-                        //document.body.removeChild(link);
-                    });
-                }
-            }
-            var get_file_from_cache=function(){
-                if('caches' in window){
-                    caches.open('VM').then(
-                        cache => {
-                            cache.match(m.Table+"-"+rid+"-"+tag).then(response => {
-                                if(response){
-                                    $vm.request({api:m.api,cmd:"file",id:rid,table:m.Table,uid:record.UID, field:tag,filename:filename,datetime:1,options:m.options},function(res){
-                                        var dtA=new Date(response.headers.get('last-modifie')).getTime();
-                                        var dtB=new Date(res.result).getTime();
-                                        dtA=dtA-dtA%1000;
-                                        dtB=dtB-dtB%1000;
-                                        if(dtA==dtB){
-                                            response.blob().then(function(myBlob) {
-                                                var link=document.createElement('a');
-                                                var url = window.URL || window.webkitURL;
-                                                link.href=url.createObjectURL(myBlob);
-                                                link.download=filename;
-                                                link.click();
-                                            });
-                                        }
-                                        else get_file_from_server();
-                                    })
-                                }
-                                else{
-                                    get_file_from_server();
-                                }
-                            })
-                        }
-                    )
-                }
-            }
-            get_file_from_cache();
-        }
-        else alert("No file was found on server.")
-    });
-    $('#x_'+tag+'__ID').on('click',function(){
-        $('#link_'+tag+'__ID').html('');
-        $('#x_'+tag+'__ID').hide();
-        record.Data[tag]="";
-        $('#F__ID input[name='+tag+']').val('');
-    })
-    $('#F__ID input[name='+tag+']').on('change',function(){
-        if($(this).val()!='') $('#x_'+tag+'__ID').show();
-    });
 }
 //-------------------------------
 m.set_image_url=function(tag){
@@ -165,7 +73,7 @@ m.set_image_url=function(tag){
         else{
             var expires=7*24*3600;
             var url=record.Table+"/"+record.UID+"-"+tag+"-"+filename;
-            $vm.request({api:m.api,cmd:"s3_download_url",id:record._id,table:record.Table,filename:filename,url:url,expires:expires,options:m.options},function(res){
+            $vm.request({cmd:"s3_download_url",id:record._id,table:record.Table,filename:filename,url:url,expires:expires},function(res){
                 if(res.status=="np"){
                     alert("No permission.")
                     return;
@@ -189,7 +97,6 @@ m.submit=function(event){
     //--------------------------------------------------------
     event.preventDefault();
     $('#submit__ID').hide();
-    $('#draft__ID').hide();
     //--------------------------------------------------------
     var data={};
     var index={};
@@ -206,28 +113,16 @@ m.submit=function(event){
     }
     delete data[""];
     var file=$vm.serialize_file('#F__ID');
-    var big_file_size=0;
-    var FN=0; $('#F__ID').find('input[type=file]').each(function(evt){ 
-        if(this.files.length==1) FN++; 
-        //*******************
-        if(this.files.length==1 && this.files[0].size>20000000){
-            big_file_size=1;
-            alert('File size must be less than 20M.');
-            return false;                
-        }
-        //*******************
-    });
-    if(big_file_size==1) return;
-
+    var FN=0; $('#F__ID').find('input[type=file]').each(function(evt){ if(this.files.length==1) FN++; });
     var r=true;
     if(m.before_submit!=undefined) r=m.before_submit(data,index);
-    if(r==false){$('#submit__ID').show(); $('#draft__ID').show(); return;}
+    if(r==false){$('#submit__ID').show(); return;}
     //--------------------------------------------------------
     var rid=undefined; if(m.input!=undefined && m.input.record!=undefined) rid=m.input.record._id;
     if(rid==undefined){
         var i_cmd="insert";
         if(m.cmd_type=='table') i_cmd='insert-table';
-        $vm.request({api:m.api,cmd:i_cmd,table:m.Table,data:data,index:index,file:file,options:m.options},function(res){
+        $vm.request({cmd:i_cmd,table:m.Table,data:data,index:index,file:file},function(res){
             if(res.status=="np"){
                 alert("No permission to insert a new record in to the database.");
                 if(m.input!=undefined && m.input.goback!=undefined){
@@ -244,7 +139,11 @@ m.submit=function(event){
                 //if(m.change_status==undefined) m.change_status=0;
                 m.change_status++;
                 if(m.input.goback!=undefined) window.history.go(-1);            //from grid
-                else console.log('Your data has been successfully submitted');  //$vm.alert('Your data has been successfully submitted');    //standalone
+                else { 
+                    //Special for progress form                   
+                    $vm.refresh=1;
+                    window.history.go(-1);       //from form
+                }
             }
             if(FN==0) after_submit();
             else{
@@ -262,7 +161,7 @@ m.submit=function(event){
         if(m.cmd_type=='p1') cmd='update-p1';
         if(m.cmd_type=='p2') cmd='update-p2';
         if(m.cmd_type=='table') cmd='update-table';
-        $vm.request({api:m.api,cmd:cmd,id:rid,table:m.Table,data:data,index:index,file:file,options:m.options},function(res){
+        $vm.request({cmd:cmd,id:rid,table:m.Table,data:data,index:index,file:file},function(res){
             //-----------------------------
             if(res.status=="lk"){
                 $vm.alert("This record is locked.");
@@ -297,152 +196,8 @@ m.submit=function(event){
     }
 }
 //--------------------------------------------------------
-m.submit_s0=function(event){
-    //-------------------------------------
-    event.preventDefault();
-    $('#submit__ID').hide();
-    $('#draft__ID').hide();
-    //-------------------------------------
-    var rid=undefined; if(m.input!=undefined && m.input.record!=undefined) rid=m.input.record._id;
-    var data={};
-    var files=[];
-    var index={};
-    //-------------------------------------
-    //data
-    var data_new=$vm.serialize('#F__ID');
-    if(m.input!=undefined && m.input.record!=undefined){
-        for(k in m.input.record.Data){
-            data[k]=m.input.record.Data[k];
-        }
-    }
-    if(data_new!=undefined){
-        for(k in data_new){
-            data[k]=data_new[k];
-        }
-    }
-    delete data[""];
-    //-------------------------------------
-    //index
-    var r=true;
-    if(m.before_submit!=undefined) r=m.before_submit(data,index);
-    if(r==false){ $('#submit__ID').show(); $('#draft__ID').show(); return;}
-    //-------------------------------------
-    //after files
-    var after_read_files=function(){
-        if(rid==undefined){
-            var i_cmd="insert";
-            $vm.request({api:m.api,cmd:i_cmd,table:m.Table,data:data,index:index,fdata:files,options:m.options},
-                function(res){
-                    close_model__ID();
-                    if(res.status=="np"){
-                        alert("No permission to insert a new record in to the database.");
-                        if(m.input!=undefined && m.input.goback!=undefined){
-                            $vm.refresh=1;
-                            window.history.go(-1);       //from grid
-                        }
-                        return;
-                    }
-                    var after_submit=function(){
-                        if(m.after_insert!=undefined){
-                            m.after_insert(data,res); return;
-                        }
-                        $vm.refresh=1;
-                        m.change_status++;
-                        if(m.input.goback!=undefined) window.history.go(-1);            //from grid
-                        else $vm.alert('Your data has been successfully submitted');    //standalone
-                    }
-                    after_submit();
-                    //-----------------------------
-                },
-                function(loaded, total){
-                    if(FN!=0){
-                        $("#msg__ID").text("Uploading... "+(100*loaded/total)+"%");
-                    }
-                }
-            )
-                
-        }
-        else if(rid!=undefined){
-            var cmd="update";
-            if(m.cmd_type=='p1') cmd='update-p1';
-            if(m.cmd_type=='p2') cmd='update-p2';
-            if(m.cmd_type=='table') cmd='update-table';
-
-            $vm.request(
-                {api:m.api,cmd:cmd,id:rid,table:m.Table,data:data,index:index,fdata:files,options:m.options},
-                function(res){
-                    close_model__ID();
-                    //-----------------------------
-                    if(res.status=="lk"){
-                        $vm.alert("This record is locked.");
-                        return;
-                    }
-                    //-----------------------------
-                    if(res.status=="np"){
-                        alert("No permission to update this record.");
-                        return;
-                    }
-                    //-----------------------------
-                    var after_submit=function(){
-                        if(m.after_update!=undefined){
-                            m.after_update(data,res); return;
-                        }
-                        $vm.refresh=1;
-                        m.change_status++;
-                        if(rid!=undefined) window.history.go(-1);                       //modify
-                    }
-                    //-----------------------------
-                    after_submit();
-                    //-----------------------------
-                },
-                function(loaded, total){
-                    if(FN!=0){
-                        $("#msg__ID").text("Uploading... "+(100*loaded/total)+"%");
-                    }
-                }
-            );
-        }
-    }
-    //-------------------------------------
-    //files
-    var FN=0; 
-    $('#F__ID').find('input[type=file]').each(function(evt){if(this.files.length==1){FN++;}});
-    var FNN=FN;
-    if(FNN!=0){
-        open_model__ID();
-        $("#msg__ID").text("Uploading...");
-        
-        $('#F__ID').find('input[type=file]').each(function(evt){ 
-            if(this.files.length==1){
-                var r={};
-                r.field_name=this.name;
-                r.file_name=this.files[0].name;
-                if(this.files[0].size>20000000){ 
-                    alert('File size must be less than 20M.');
-                    close_model__ID();
-                    return false;                
-                }
-                var reader = new FileReader();
-                reader.onload = (function(e) {
-                    var c=e.target.result;
-                    var s=$vm.getB64Str(c);
-                    r.b64str=s;
-                    files.push(r);
-                    FNN--;
-                    if(FNN==0) after_read_files();
-                });
-                reader.readAsArrayBuffer(this.files[0]);
-            }
-        });
-    }
-    else{
-        after_read_files();
-    }
-    //-------------------------------------
-}
-//--------------------------------------------------------
 $('#D__ID').on('load',function(){ m.load();})
-$('#F__ID').submit(function(event){if(m.storage_type=='s0') m.submit_s0(event); else m.submit(event);})
+$('#F__ID').submit(function(event){m.submit(event);})
 //--------------------------------------------------------
 $('#delete__ID').on('click', function(){
     var record=m.input.record;    if(record==undefined) return;
@@ -450,7 +205,7 @@ $('#delete__ID').on('click', function(){
     if(confirm("Are you sure to delete?\n")){
         var d_cmd="delete";
         if(m.cmd_type=='table') cmd='delete-table';
-        $vm.request({api:m.api,cmd:d_cmd,id:rid,table:m.Table,options:m.options},function(res){
+        $vm.request({cmd:d_cmd,id:rid,table:m.Table},function(res){
             //-----------------------------
             if(res.status=="lk"){
                 $vm.alert("This record is locked.");
--- modules/module-list.js
+++ modules/module-list.js
@@ -13,6 +13,37 @@
     m[p+"contact-data"]      ={url:H+"/form-grid/survey/contact-data.html",Table:"backpainsleep-contact-v2",form_module:"contact-form",router:1,taskname:"Contact details" };
     m[p+"contact-form"]      ={url:H+"/form-grid/survey/contact-form.html",Table:"backpainsleep-contact-v2"};
     
+    m[p+"participant-data"] ={url:H+"/form-grid/participant/participant-data.html",Table:"participant-backpainsleep-v2"
+                            ,form_module:"participant-form",
+                            child_panel:"panel-child",
+                            online_questionnaire_bl:"oq-bl",
+                            online_questionnaire_fu:"oq-fu",
+                            participant_id:{field1:"Subject_ID",field2:"Subject_Initials"},
+                            participant_export:"ID,Subject_ID,Screening_Number,Randomisation_Number,Subject_Initials,Gender,DOB",
+                            task_name:"Participants Data",
+    };                   
+    m[p+"participant-form"] ={url:H+"/form-grid/participant/participant-form.html",Table:"participant-backpainsleep-v2",task_name:"Participant's Details"};
+
+    m[p+"oq-bl"] ={url:I+"/oq-bl.html"};
+    m[p+"oq-fu"] ={url:I+"/oq-fu.html"};
+    m[p+"panel-main"]=    	        {url:H+"/panels/main.html",router:1,task_name:"Study Panel"};
+    m[p+"panel-child"]=              {url:H+"/panels/child.html",task_name:"Data Entry"};
+    m[p+"panel-oq-bl"]=              {url:H+"/panels/oq-bl.html"};
+    m[p+"panel-oq-fu"]=              {url:H+"/panels/oq-fu.html"};
+
+    m[p+"quest-1-bl-data"]               ={url:H+"/form-grid/backend/ess-data.html",Table:"quest-1-bl-backpainsleep-v2",form_module:"quest-1-bl-form",task_name:"Baseline - Questionnaire 1"};
+    m[p+"quest-1-bl-form"]               ={url:H+"/form-grid/backend/ess-form.html",Table:"quest-1-bl-backpainsleep-v2",task_name:"Baseline - Questionnaire 1"};
+    m[p+"quest-2-bl-data"]               ={url:H+"/form-grid/backend/ess-data.html",Table:"quest-2-bl-backpainsleep-v2",form_module:"quest-2-bl-form",task_name:"Baseline - Questionnaire 2"};
+    m[p+"quest-2-bl-form"]               ={url:H+"/form-grid/backend/ess-form.html",Table:"quest-2-bl-backpainsleep-v2",task_name:"Baseline - Questionnaire 2"};
+    m[p+"quest-3-bl-data"]               ={url:H+"/form-grid/backend/ess-data.html",Table:"quest-3-bl-backpainsleep-v2",form_module:"quest-3-bl-form",task_name:"Baseline - Questionnaire 3"};
+    m[p+"quest-3-bl-form"]               ={url:H+"/form-grid/backend/ess-form.html",Table:"quest-3-bl-backpainsleep-v2",task_name:"Baseline - Questionnaire 3"};
+
+    m[p+"quest-1-fu-data"]               ={url:H+"/form-grid/backend/ess-data.html",Table:"quest-1-fu-backpainsleep-v2",form_module:"quest-1-fu-form",task_name:"Follow-up - Questionnaire 1"};
+    m[p+"quest-1-fu-form"]               ={url:H+"/form-grid/backend/ess-form.html",Table:"quest-1-fu-backpainsleep-v2",task_name:"Follow-up - Questionnaire 1"};
+    m[p+"quest-2-fu-data"]               ={url:H+"/form-grid/backend/ess-data.html",Table:"quest-2-fu-backpainsleep-v2",form_module:"quest-2-fu-form",task_name:"Follow-up - Questionnaire 2"};
+    m[p+"quest-2-fu-form"]               ={url:H+"/form-grid/backend/ess-form.html",Table:"quest-2-fu-backpainsleep-v2",task_name:"Follow-up - Questionnaire 2"};
+    m[p+"quest-3-fu-data"]               ={url:H+"/form-grid/backend/ess-data.html",Table:"quest-3-fu-backpainsleep-v2",form_module:"quest-3-fu-form",task_name:"Follow-up - Questionnaire 3"};
+    m[p+"quest-3-fu-form"]               ={url:H+"/form-grid/backend/ess-form.html",Table:"quest-3-fu-backpainsleep-v2",task_name:"Follow-up - Questionnaire 3"};
 
     //if(window.location.toString().indexOf('tb=demo')!=-1){
         for(p in $vm.module_list){
