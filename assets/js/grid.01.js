64cd6f48af8c56a41486d7e64bc9b6d116ea37b2
Gunnar Unger <gunnar.unger@gmail.com>
2024-07-24 16:05:53 +1000

    update

--- assets/js/grid.01.js
+++ assets/js/grid.01.js
@@ -117,7 +117,11 @@ m.render=function(){
             if(m.records[i][b]!==undefined){
                 value=m.records[i][b];
                 //if(b=="Submit_date") value=$vm.date_to_ddmmyyyy(value.substring(0,10));
-                if(b=="Submit_date") value=new Date(value).toLocaleDateString(window.navigator.userLanguage || window.navigator.language);
+                //if(b=="Submit_date") value=new Date(value).toLocaleDateString(window.navigator.userLanguage || window.navigator.language);
+                if(b=="Submit_date") {
+                    var timepart=value.substring(11,19)
+                    value=$vm.date_to_ddmmyyyy(value.substring(0,10))+" "+timepart;
+                }
             }
             else{
                 if(m.records[i]['Data']!=undefined && m.records[i]['Data'][b]!==undefined){
--- assets/js/grid.01.multiple.js
+++ assets/js/grid.01.multiple.js
@@ -116,8 +116,12 @@ m.render=function(){
             var value="";
             if(m.records[i][b]!==undefined){
                 value=m.records[i][b];
-                if(b=="Submit_date") value=$vm.date_to_ddmmyyyy(value.substring(0,10));
-                if(b=="Update_date") value=$vm.date_to_ddmmyyyy(value.substring(0,10));
+                //if(b=="Submit_date") value=$vm.date_to_ddmmyyyy(value.substring(0,10));
+                if(b=="Submit_date") {
+                    var timepart=value.substring(11,19)
+                    value=$vm.date_to_ddmmyyyy(value.substring(0,10))+" "+timepart;
+                }
+                //if(b=="Submit_date") {value=new Intl.DateTimeFormat('en-AU',{day:'2-digit',month:'2-digit',year:'2-digit',hour:'2-digit',minute:'2-digit',second:'2-digit',}).format(value)}
             }
             else{
                 if(m.records[i]['Data']!=undefined && m.records[i]['Data'][b]!==undefined){
