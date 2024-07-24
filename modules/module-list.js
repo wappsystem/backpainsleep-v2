60bf7c64fff5dad3a4e4a4e79724f64dbdce7bb5
Gunnar Unger <gunnar.unger@gmail.com>
2024-07-25 08:45:14 +1000

    update

--- modules/module-list.js
+++ modules/module-list.js
@@ -9,6 +9,7 @@
     m[p+"not_elig1"]         ={url:H+"/form-grid/survey/not_elig1.html"},
     m[p+"thank-you"]         ={url:H+"/form-grid/survey/thank-you.html"},
     m[p+"survey-data"]       ={url:H+"/form-grid/survey/eligibility-data.html",Table:"sleepback-survey",router:1 };
+    m[p+"survey-elig-data"]  ={url:H+"/form-grid/survey/eligibility-elig-data.html",Table:"sleepback-survey",router:1 };
     m[p+"survey-all-data"]   ={url:H+"/form-grid/survey/eligibility-data.html",Table:"sleepback-survey",router:1 };
     m[p+"contact-data"]      ={url:H+"/form-grid/survey/contact-data.html",Table:"sleepback-contact",form_module:"contact-form",router:1,taskname:"Contact details" };
     m[p+"contact-form"]      ={url:H+"/form-grid/survey/contact-form.html",Table:"sleepback-contact"};
